b630f9fa80671f30356f476c9f8a99d3a0adb4ca
Jenna Salhab <salhabje@msu.edu>
2024-01-22 23:39:52 -0500

    final

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
     
-
+    eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy({"src/base.css": "base.css"});
 
     return {
